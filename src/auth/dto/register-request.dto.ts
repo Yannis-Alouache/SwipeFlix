c4880d92bd5781c2514f6277264f237998ce4627
Yannis Alouache <alouacehyannis1@gmail.com>
2024-09-27 22:03:38 +0200

    refactor: Remove unused code in register-request.dto.ts

--- src/auth/dto/register-request.dto.ts
+++ src/auth/dto/register-request.dto.ts
@@ -1,10 +1,10 @@
 import { IsEmail, IsString, Matches } from 'class-validator';
 
 export class RegisterRequestDto {
+
     @IsEmail({}, { message: 'E-Mail non valide !' })
     email: string;
 
-    @IsString()
     @Matches(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^<])[A-Za-z\d@.#$!%*?&^<]{8,15}$/,
         { message: 'Mot de passe non valide !' }
